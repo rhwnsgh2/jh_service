f5372a16e88a2157f53862db3b85a69057ebe4d0
junho <junho.ko@business-canvas.com>
2023-01-02 17:41:05 +0900

    remove conosole.log

--- src/server/api/createTodoStub.ts
+++ src/server/api/createTodoStub.ts
@@ -3,7 +3,6 @@ import type { ICreateTodo } from "./createTodo"
 export const todoListStub = [] as string[]
 
 export const createTodoStub : ICreateTodo = (value: string)=>{
-    console.log(value)
     todoListStub.push(value)
     return true
 }
